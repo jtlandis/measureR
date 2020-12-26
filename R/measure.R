6f58fccb0fe038d6fe13ef3201c9318c10bd5653
jtlandis <jtlandis314@gmail.com>
2020-12-26 14:32:06 -0500

    changing so measure will hold solts for all unit types... this way arithmatic can be checked instantly

--- R/measure.R
+++ R/measure.R
@@ -1,18 +1,26 @@
 
 measure <- setClass("measure", slots = c(value = "numeric",
-                                         type = "unit_type",
-                                         unit = "character",
-                                         prefix = "character",
-                                         scale_factor = "numeric"),
+                                         weight = "weight",
+                                         distance = "distance",
+                                         time = "time",
+                                         temperature = "temperature"),
                     prototype = list(value = numeric(0),
-                                     unit = NA_character_,
-                                     prefix = "",
-                                     scale_factor = 1))
+                                     weight = weight(),
+                                     distance = distance(),
+                                     time = time(),
+                                     temperature = temperature()))
 
 setMethod("show", "measure",
           function(object){
-            o_unit <- paste0(object@prefix, object@unit)
-            cat("measure: ", class(object@type), " (",o_unit,")", "\n", sep = "")
+            unit_l <- getUnitSlots(object)
+            o_unit <- vapply(unit_l, function(x){ paste0(getUnit(x),ifelse(abs(x@power)==1, "", paste0("^",abs(x@power))))}, FUN.VALUE = character(1))
+            numerator <- vapply(unit_l, FUN = function(x) x@power>0, FUN.VALUE = logical(1))
+            numer_ <- ifelse(sum(numerator)>0, paste0("(",o_unit[numerator],")", collapse = "*"), "1")
+            denom_ <- ifelse(sum(!numerator)>0,paste0("/",paste0("(",o_unit[!numerator],")",collapse = "*")), "")
+            the_unit <- paste0(numer_, denom_)
+            cat("measure: ",
+                #class(object@type),
+                " ", the_unit, "\n", sep = "")
             print(object@value)
             cat("\n")
           })
@@ -20,5 +28,11 @@ setMethod("show", "measure",
 setMethod("head", "measure",
           function(x, ...){
             x@value <- head(x@value, ...)
+            x
+          })
+
+setMethod("head", "measure",
+          function(x, ...){
+            x@value <- tail(x@value, ...)
             x
           })
--- R/unit_types.R
+++ R/unit_types.R
@@ -1,6 +1,24 @@
 
 
-setClass("unit_type")
+setClass("unit_type", slots = c(unit = "character",
+                                prefix = "character",
+                                scale = "numeric",
+                                power = "numeric",
+                                .active = "logical"))
+setMethod("initialize", "unit_type",
+          function(.Object, unit = NA_character_, prefix = NA_character_, scale = 1, power = 1, ...){
+            .Object@unit <- unit
+            .Object@prefix <- prefix
+            .Object@scale <- scale
+            .Object@power <- power
+            .Object
+          })
+
+is_active <- function(x) UseMethod("is_active", x)
+is_active.default <- function(x) FALSE
+is_active.unit_type <- function(x) x@power!=0&&!is.na(x@unit)
+
+
 weight <- setClass("weight", contains = "unit_type")
 distance <- setClass("distance", contains = "unit_type")
 time <- setClass("time", contains = "unit_type")
