fb03fe5ae8831f3b619d857bbe1106d5b2b77c6f
jtlandis <jtlandis314@gmail.com>
2020-12-25 23:39:08 -0500

    correcting convert.R and allowing ounces and grams units to convert

--- R/convert.R
+++ R/convert.R
@@ -6,7 +6,21 @@ setMethod("convert", signature("measure","measure"),
             if(class(object@type)!=class(to@type)) {
               rlang::abort(glue::glue("Cannot convert measure {class(object@type)} to {class(to@type)}"))
             } else {
-              to@value <- (object@value/object@scale_factor)/(to@scale_factor)
+              if(object@unit!=object@unit) rlang::warn(glue::glue("Method to convert {class(object@type)} units {object@unit} to {to@unit} has not been defined yet"))
+              to@value <- (object@value*object@scale_factor)/(to@scale_factor)
               return(to)
             }
+          })
+
+
+
+setMethod("convert", signature("ounce","gram"),
+          function(object, to){
+            to@value <- (object@value*object@scale_factor)*(28.3495/to@scale_factor)
+            to
+          })
+setMethod("convert", signature("gram","ounce"),
+          function(object, to){
+            to@value <- (object@value*object@scale_factor)*(0.035274/to@scale_factor)
+            to
           })
