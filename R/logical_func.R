13708adb21f8b30e44335dd11ca4bebcd9444350
jtlandis <jtlandis314@gmail.com>
2020-12-27 04:10:45 -0500

    constants and signatures

--- R/logical_func.R
+++ R/logical_func.R
@@ -19,14 +19,25 @@ is_Temperature.measure <- function(x) any(vapply(getUnitSlots(x), is_Temperature
 
 setGeneric("identical_measures", valueClass = "logical", function(e1, e2) standardGeneric("identical_measures"))
 setMethod("identical_measures",
-          signature(e1 = "measure", e2 = "measure"),
+          signature(e1 = "Measure", e2 = "Measure"),
           function(e1, e2){
-            class(e1@type)==class(e2@type)&&e1@unit==e2@unit&&e1@prefix==e2@prefix
+            e1_l <- getUnitSlots(e1)
+            e2_l <- getUnitSlots(e2)
+            if(!all(names(e1_l)%in%names(e2_l))) return(FALSE)
+            e2_l <- e2_l[names(e1_l)]
+            logi <- Map(function(x, y){
+              x@unit==y@unit&&x@prefix==y@prefix&&x@power==y@power&&x@scale==y@scale
+            }, x = e1_l, y = e2_l)
+            all(unlist(logi))
+          })
+setMethod("identical_measures", signature("Unit_type","Unit_type"),
+          function(e1, e2){
+            class(e1)==class(e2)&&e1@unit==e2@unit&&e1@prefix==e2@prefix&&e1@power==e2@power&&e1@scale==e2@scale
           })
 
 setGeneric("convertable", valueClass = "logical", function(e1, e2) standardGeneric("convertable"))
 setMethod("convertable",
-          signature(e1 = "measure", e2 = "measure"),
+          signature(e1 = "Measure", e2 = "Measure"),
           function(e1, e2){
-            class(e1@type)==class(e2@type)
+            all(names(getUnitSlots(e1))%in%names(getUnitSlots(e2)))
           })
--- R/unit_types.R
+++ R/unit_types.R
@@ -18,7 +18,7 @@ is_active.default <- function(x) FALSE
 is_active.Unit_type <- function(x) x@power!=0&&!is.na(x@unit)
 
 setGeneric("getUnitSlots", valueClass = "list", function(object) standardGeneric("getUnitSlots"))
-setMethod("getUnitSlots", signature = "measure",
+setMethod("getUnitSlots", signature = "Measure",
           function(object){
             l_ <- Map(slot, object = list(object), name = slotNames(object))
             l_ <- l_[vapply(l_, is_active, FUN.VALUE = logical(1))]
@@ -33,6 +33,7 @@ setMethod("getUnit", signature = "Unit_type",
 setMethod("getUnit", signature = "Measure",
           function(object){
             unit_l <- getUnitSlots(object)
+            if(length(unit_l)==0) return("constant")
             o_unit <- vapply(unit_l, function(x){ paste0(getUnit(x),ifelse(abs(x@power)==1, "", paste0("^",abs(x@power))))}, FUN.VALUE = character(1))
             numerator <- vapply(unit_l, FUN = function(x) x@power>0, FUN.VALUE = logical(1))
             numer_ <- ifelse(sum(numerator)>0, paste0("(",o_unit[numerator],")", collapse = "*"), "1")
