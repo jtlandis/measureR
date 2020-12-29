c99bdc0f1bab905216e68f46357477ea1fa5a4c0
jtlandis <jtlandis314@gmail.com>
2020-12-29 02:47:31 -0500

    Ounce classes

--- R/ounce.R
+++ R/ounce.R
@@ -1,45 +1,60 @@
 
-
-setClass("ounce",
-         contains = "measure",
-         prototype = list(type = weight(),
-                          prefix = "oz",
-                          unit = "ounce"))
-
-setGeneric("ounce", valueClass = "ounce", function(object) standardGeneric("ounce"))
-setMethod("ounce", signature("missing"), function(object) new("ounce"))
-setMethod("ounce", signature("numeric"), function(object) new("ounce", value = object))
-setMethod("ounce", signature("measure"), function(object) convert(object, ounce()))
-setMethod("show", "ounce",
-          function(object){
-            cat("measure: ", class(object@type), "(",object@prefix,")","\n", sep = "")
-            print(object@value)
-            cat("\n")
+#
+setClass("Ounce",
+         contains = "Weight")
+setMethod("initialize", "Ounce",
+          function(.Object, prefix = "oz", ...) {
+            .Object <- callNextMethod(.Object, prefix = prefix, ...)
+            .Object@unit <- "ounce"
+            .Object@power <- 1
+            .Object
           })
 
+setMethod("getUnit", signature = "Ounce",
+          function(object){
+            paste0(object@prefix)
+          })
 
-setClass("pound",
-         contains = "ounce",
-         prototype = list(prefix = "lb",
-                          scale_factor = 16))
-
-setGeneric("pound", valueClass = "pound", function(object) standardGeneric("pound"))
-setMethod("pound", signature("missing"), function(object) new("pound"))
-setMethod("pound", signature("numeric"), function(object) new("pound", value = object))
-setMethod("pound", signature("measure"), function(object) convert(object, pound()))
+# ounce ----
+#' @export
+setGeneric("ounce", valueClass = "Measure", function(object) standardGeneric("ounce"))
+setMethod("ounce", signature("missing"), function(object) measure(Weight = new("Ounce")))
+setMethod("ounce", signature("numeric"), function(object) measure(.Data =object, Weight = new("Ounce")))
+setMethod("ounce", signature("measure"), function(object) convert(object, new("Ounce")))
 
 
-setClass("dram",
-         contains = "ounce",
-         prototype = list(prefix = "dr",
-                          scale_factor = 1/16))
 
-setGeneric("dram", valueClass = "dram", function(object) standardGeneric("dram"))
-setMethod("dram", signature("missing"), function(object) new("dram"))
-setMethod("dram", signature("numeric"), function(object) new("dram", value = object))
-setMethod("dram", signature("measure"), function(object) convert(object, dram()))
 
-# lb <- pound(1)
-# dram(lb)
-# ounce(lb)
-# pound(lb)
+# pound ----
+
+setClass("Pound",
+         contains = "Ounce")
+setMethod("initialize", "Pound",
+          function(.Object, prefix = "lb", ...){
+            .Object <- callNextMethod(.Object, prefix = prefix, ...)
+            .Object@scale <- 16
+            .Object
+          })
+#' @export
+setGeneric("pound", valueClass = "Measure", function(object) standardGeneric("pound"))
+setMethod("pound", signature("missing"), function(object) measure(Weight = new("Pound")))
+setMethod("pound", signature("numeric"), function(object) measure(.Data =object, Weight = new("Pound")))
+setMethod("pound", signature("measure"), function(object) convert(object, new("Pound")))
+
+# pound ----
+
+setClass("Dram",
+         contains = "Ounce")
+setMethod("initialize", "Dram",
+          function(.Object, prefix = "dr", ...){
+            .Object <- callNextMethod(.Object, prefix = prefix, ...)
+            .Object@scale <- 1/16
+            .Object
+          })
+#' @export
+setGeneric("dram", valueClass = "Measure", function(object) standardGeneric("dram"))
+setMethod("dram", signature("missing"), function(object) measure(Weight = new("Dram")))
+setMethod("dram", signature("numeric"), function(object) measure(.Data =object, Weight = new("Dram")))
+setMethod("dram", signature("measure"), function(object) convert(object, new("Dram")))
+
+
