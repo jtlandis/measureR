ef176875b9f4916773c1abb4b61d3debb6e86c84
jtlandis <jtlandis314@gmail.com>
2020-12-29 15:06:32 -0500

    hypothetical 'new' class structure. Unit, and System classes, UnitSystem contains these two. UnitSystem to replace Unit_Type

---
+++ R/experimental.R
@@ -0,0 +1,68 @@
+
+"%missing%" <- function(a,b) if(!missing(a)) a else b
+setClass("Unit", slots = c(power = "numeric"))
+setClass("Weight", contains = "Unit")
+
+setMethod("initialize", "Unit",
+          function(.Object, power = 0){
+            .Object@power <- power
+            .Object
+          })
+setClass("System", slots = c(scale = "numeric"))
+setMethod("initialize", "System",
+          function(.Object, .Data = "Default_System_Value", scale = 1){
+            .Object@scale <- scale
+            .Object
+          })
+
+
+setClass("UnitSystem", contains = "character",
+         slots = c(unit = "Unit",system = "System"))
+setMethod("initialize", "UnitSystem",
+          function(.Object, .Data = character(0), unit, system){
+            .Object@.Data <- .Data
+            .Object@unit <- unit %missing% new("Unit")
+            .Object@system <- system %missing% new("System")
+            .Object
+          })
+
+
+metric_prefix <- c("",
+                  "da","h","k","M","G","T","P","E","Z","Y",
+                  "d","c","m","u","n","p","f","a","z","y")
+
+setClass("Metric", contains = "System")
+setMethod("initialize", "Metric",
+          function(.Object, .Data = "Metric"){
+            if(!is.element(.Data, metric_prefix)){
+              abort(glue("Metric Class must be initialized with one of",
+                         " {paste0(\"\\\"\",metric_prefix,\"\\\"\", collapse = \", \")}"))
+            }
+            .Object@scale <- 10^(switch(.Data,
+                                        da = 1L,h = 2L,k = 3L, M = 6L, G = 9L,
+                                        `T` = 12L, P = 15L, E = 18L, Z= 21L, Y = 24L,
+                                        d = -1L, c = -2L, m = -3L, u = -6L, n = -9L,
+                                        p = -12L, f = -15L, a = -18L, z = -21L, y = -24L,
+                                        0L
+                                        ))
+            .Object
+          })
+
+
+
+
+setClass("Gram_", contains = "UnitSystem")
+setMethod("initialize", "Gram_",
+          function(.Object, unit = "g"){
+            munit <- paste0(metric_prefix,"g")
+            if(!is.element(unit, munit)){
+              abort(glue("Gram Class must be initialized with one of",
+                         " {paste0(\"\\\"\",metric_prefix,\"g\",\"\\\"\", collapse = \", \")}"))
+            }
+            prefix <- gsub("g$", "", unit)
+            .Object <- callNextMethod(.Object, unit = new("Weight"), system = new("Metric", prefix))
+            .Object
+          })
+
+new("Gram_")
+
