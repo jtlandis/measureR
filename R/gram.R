476ef455b9e1b46df55aa67e8da5937b4ce62f51
jtlandis <jtlandis314@gmail.com>
2020-12-30 01:57:49 -0500

    different constructors to reliably name the @info UnitSystems

--- R/gram.R
+++ R/gram.R
@@ -12,47 +12,47 @@ setMethod("initialize", "Gram",
 
 
 #' @export
-gram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "g") else Constructor(object = x, Class = "Gram", unit = "g")
+gram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "g") else Const_Weight(object = x, Class = "Gram", unit = "g")
 #' @export
-dagram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "dag") else Constructor(object = x, Class = "Gram", unit = "dag")
+dagram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "dag") else Const_Weight(object = x, Class = "Gram", unit = "dag")
 #' @export
-hgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "hg") else Constructor(object = x, Class = "Gram", unit = "hg")
+hgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "hg") else Const_Weight(object = x, Class = "Gram", unit = "hg")
 #' @export
-kgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "kg") else Constructor(object = x, Class = "Gram", unit = "kg")
+kgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "kg") else Const_Weight(object = x, Class = "Gram", unit = "kg")
 #' @export
-Mgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Mg") else Constructor(object = x, Class = "Gram", unit = "Mg")
+Mgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Mg") else Const_Weight(object = x, Class = "Gram", unit = "Mg")
 #' @export
-Ggram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Gg") else Constructor(object = x, Class = "Gram", unit = "Gg")
+Ggram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Gg") else Const_Weight(object = x, Class = "Gram", unit = "Gg")
 #' @export
-Tgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Tg") else Constructor(object = x, Class = "Gram", unit = "Tg")
+Tgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Tg") else Const_Weight(object = x, Class = "Gram", unit = "Tg")
 #' @export
-Pgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Pg") else Constructor(object = x, Class = "Gram", unit = "Pg")
+Pgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Pg") else Const_Weight(object = x, Class = "Gram", unit = "Pg")
 #' @export
-Egram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Eg") else Constructor(object = x, Class = "Gram", unit = "Eg")
+Egram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Eg") else Const_Weight(object = x, Class = "Gram", unit = "Eg")
 #' @export
-Zgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Zg") else Constructor(object = x, Class = "Gram", unit = "Zg")
+Zgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Zg") else Const_Weight(object = x, Class = "Gram", unit = "Zg")
 #' @export
-Ygram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "Yg") else Constructor(object = x, Class = "Gram", unit = "Yg")
+Ygram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "Yg") else Const_Weight(object = x, Class = "Gram", unit = "Yg")
 #' @export
-dgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "dg") else Constructor(object = x, Class = "Gram", unit = "dg")
+dgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "dg") else Const_Weight(object = x, Class = "Gram", unit = "dg")
 #' @export
-cgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "cg") else Constructor(object = x, Class = "Gram", unit = "cg")
+cgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "cg") else Const_Weight(object = x, Class = "Gram", unit = "cg")
 #' @export
-mgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "mg") else Constructor(object = x, Class = "Gram", unit = "mg")
+mgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "mg") else Const_Weight(object = x, Class = "Gram", unit = "mg")
 #' @export
-ugram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "ug") else Constructor(object = x, Class = "Gram", unit = "ug")
+ugram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "ug") else Const_Weight(object = x, Class = "Gram", unit = "ug")
 #' @export
-ngram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "ng") else Constructor(object = x, Class = "Gram", unit = "ng")
+ngram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "ng") else Const_Weight(object = x, Class = "Gram", unit = "ng")
 #' @export
-pgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "pg") else Constructor(object = x, Class = "Gram", unit = "pg")
+pgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "pg") else Const_Weight(object = x, Class = "Gram", unit = "pg")
 #' @export
-fgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "fg") else Constructor(object = x, Class = "Gram", unit = "fg")
+fgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "fg") else Const_Weight(object = x, Class = "Gram", unit = "fg")
 #' @export
-agram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "ag") else Constructor(object = x, Class = "Gram", unit = "ag")
+agram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "ag") else Const_Weight(object = x, Class = "Gram", unit = "ag")
 #' @export
-zgram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "zg") else Constructor(object = x, Class = "Gram", unit = "zg")
+zgram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "zg") else Const_Weight(object = x, Class = "Gram", unit = "zg")
 #' @export
-ygram <- function(x) if(missing(x)) Constructor(Class = "Gram", unit = "yg") else Constructor(object = x, Class = "Gram", unit = "yg")
+ygram <- function(x) if(missing(x)) Const_Weight(Class = "Gram", unit = "yg") else Const_Weight(object = x, Class = "Gram", unit = "yg")
 
 
 
