0702762e10c623b2132d3653cb8557f201a5d0e3
jtlandis <jtlandis314@gmail.com>
2021-03-17 23:00:22 -0400

    cast_measure

--- R/helpers.R
+++ R/helpers.R
@@ -22,3 +22,19 @@ setMethod("getInfo", signature("Measure", "character"),
             object@info[names]
           })
 
+
+cast_measure <- function(x, to, no_common_error = FALSE,
+                         non_similar_error = FALSE,
+                         non_identical_error = FALSE, ...) {
+  x_unit <- unit(x)
+  to_unit <- unit(to)
+  #find common UnitTypes
+  to_convert <- units2convert(x, to,
+                              no_common_error = no_common_error,
+                              non_similar_error = non_similar_error,
+                              non_identical_error = non_identical_error)
+  if(length(to_convert)>0){
+    x <- reduce(c(list(x), to_unit[to_convert]), convert)
+  }
+  x
+}
