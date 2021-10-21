8c47ec0c2a11b82c67b8cbb97ad0fa82f9aa4158
Justin Landis <jtlandis@pop-os.localdomain>
2021-10-21 17:04:59 -0400

    Condensing code

--- R/msr-class-UnitSystem-Weight.R
+++ R/msr-class-UnitSystem-Weight.R
@@ -25,17 +25,122 @@ setMethod("initialize", "Gram",
           })
 
 
-gram <- function(x = double()) {
+#' @export
+gram <- function(x) {
   msr_cast(x, new("Gram", unit = "g"))
 }
-
-dagram <- function(x = double()) {
+#' @export
+dagram <- function(x) {
   msr_cast(x, new("Gram", unit = "dag"))
+}
+#' @export
+hgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "hg"))
+}
+#' @export
+kgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "kg"))
+}
+#' @export
+Mgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Mg"))
+}
+#' @export
+Ggram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Gg"))
+}
+#' @export
+Tgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Tg"))
+}
+#' @export
+Pgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Pg"))
+}
+#' @export
+Egram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Eg"))
+}
+#' @export
+Zgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Zg"))
+}
+#' @export
+Ygram <- function(x) {
+  msr_cast(x, new("Gram", unit = "Yg"))
+}
+#' @export
+dgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "dg"))
+}
+#' @export
+cgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "cg"))
+}
+#' @export
+mgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "mg"))
+}
+#' @export
+ugram <- function(x) {
+  msr_cast(x, new("Gram", unit = "ug"))
+}
+#' @export
+ngram <- function(x) {
+  msr_cast(x, new("Gram", unit = "ng"))
+}
+#' @export
+pgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "pg"))
+}
+#' @export
+fgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "fg"))
+}
+#' @export
+agram <- function(x) {
+  msr_cast(x, new("Gram", unit = "ag"))
+}
+#' @export
+zgram <- function(x) {
+  msr_cast(x, new("Gram", unit = "zg"))
+}
+#' @export
+ygram <- function(x) {
+  msr_cast(x, new("Gram", unit = "yg"))
 }
 
 
 
 
 
+ounce_scale <- function(x){
+  switch(x, oz = 1, lb = 16, dr = 1/16, ton = 32000)
+}
+setClass("Ounce", contains = c("Weight"))
+setMethod("initialize", "Ounce",
+          function(.Object, ..., unit = "oz"){
+            scale <- ounce_scale(unit)
+            .Object <- callNextMethod(.Object, .Data = unit, scale = scale, power = 1)
+            .Object
+          })
+
+
+#' @export
+ounce <- function(x) {
+  msr_cast(x, new("Ounce", unit = "oz"))
+}
+#' @export
+pound <- function(x) {
+  msr_cast(x, new("Ounce", unit = "lb"))
+}
+#' @export
+dram <- function(x) {
+  msr_cast(x, new("Ounce", unit = "dr"))
+}
+#' @export
+ton <- function(x) {
+  msr_cast(x, new("Ounce", unit = "ton"))
+}
 
 
