1745b3d402da703bdedcc2ea74c22bf96690af8e
Justin Landis <jtlandis@pop-os.localdomain>
2021-10-21 17:25:09 -0400

    moving meter class

---
+++ R/msr-class-UnitSystem-Distance.R
@@ -0,0 +1,109 @@
+#' @include msr-class-UnitSystem-.R msr_cast.R
+
+setClass("Distance", contains = "UnitSystem")
+setMethod("initialize", "Distance",
+          function(.Object, ...) {
+            .Object <- callNextMethod(.Object, ...)
+            .Object@type <- "Distance"
+            .Object
+          })
+
+setMethod("msr_cast", signature("numeric", "Distance"),
+          function(object, to) {
+            new("Measure", .Data = object, unit = UnitList(Distance = to))
+          })
+
+setClass("Meter", contains = c("Distance"))
+setMethod("initialize", "Meter",
+          function(.Object, ..., unit = "m"){
+            scale <- metric_scale(gsub("m$","",unit))
+            .Object <- callNextMethod(.Object, .Data = unit, scale = scale, power = 1)
+            .Object
+          })
+
+
+#' @export
+meter <- function(x) {
+  msr_cast(x, new("Meter", unit = "m"))
+}
+#' @export
+dameter <- function(x) {
+  msr_cast(x, new("Meter", unit = "dam"))
+}
+#' @export
+hmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "hm"))
+}
+#' @export
+kmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "km"))
+}
+#' @export
+Mmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Mm"))
+}
+#' @export
+Gmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Gm"))
+}
+#' @export
+Tmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Tm"))
+}
+#' @export
+Pmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Pm"))
+}
+#' @export
+Emeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Em"))
+}
+#' @export
+Zmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Zm"))
+}
+#' @export
+Ymeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "Ym"))
+}
+#' @export
+dmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "dm"))
+}
+#' @export
+cmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "cm"))
+}
+#' @export
+mmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "mm"))
+}
+#' @export
+umeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "um"))
+}
+#' @export
+nmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "nm"))
+}
+#' @export
+pmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "pm"))
+}
+#' @export
+fmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "fm"))
+}
+#' @export
+ameter <- function(x) {
+  msr_cast(x, new("Meter", unit = "am"))
+}
+#' @export
+zmeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "zm"))
+}
+#' @export
+ymeter <- function(x) {
+  msr_cast(x, new("Meter", unit = "ym"))
+}
+
